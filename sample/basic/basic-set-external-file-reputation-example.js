c56c13219a53f4634cd3ad9a0d0e4d47c3a6b309
vrevigli <Valentin_ReviglioCanepa@McAfee.com>
2019-03-07 14:14:49 -0300

    The sample first validates that the external reputation is not conflictive

--- sample/basic/basic-set-external-file-reputation-example.js
+++ sample/basic/basic-set-external-file-reputation-example.js
@@ -3,6 +3,7 @@
 var common = require('../common')
 var dxl = common.require('@opendxl/dxl-client')
 var tie = common.require('@opendxl/dxl-tie-client')
+var MessageUtils = common.require('@opendxl/dxl-bootstrap').MessageUtils
 var HashType = tie.HashType
 var TieClient = tie.TieClient
 var TrustLevel = tie.TrustLevel
@@ -24,22 +25,54 @@ var FILE_SHA256 = '142e1d688ef0568370c37187fd9f2351d7ddeda574f8bfa9b0fa4ef42db85
 // when the connection has been established
 client.connect(function() {
     var tieClient = new TieClient(client)
+        // The trust level that the external provider wants to set to a especific file
+    var externalTrustLevel = TrustLevel.KNOWN_TRUSTED
     var hashes = {}
-    hashes[HashType.MD5] = FILE_MD5
-    hashes[HashType.SHA1] = FILE_SHA1
-    hashes[HashType.SHA256] = FILE_SHA256
+    hashes[HashType.MD5] = 'e872f1695c189c1efa656038073aa310' //FILE_MD5
+    hashes[HashType.SHA1] = 'bd3987cd8242f4e50fb99b120f16be049ddb3685' //FILE_SHA1
+    hashes[HashType.SHA256] = '84e97beb57b6410a5e8ba7871907a95b42dc78afef07e9da66ba1d8c05abffd3' //FILE_SHA256
 
-    // Set the External reputation for the file to Known Trusted
-    tieClient.setExternalFileReputation(
-        function() {
-            // Destroy the client - frees up resources so that the application
-            // stops running
-            client.destroy();
-            console.log('Event sent')
+    // Request reputation for notepad
+    tieClient.getFileReputation(
+        function(error, notepadReputation) {
+            if (error) {
+                // Destroy the client - frees up resources so that the application
+                // stops running
+                client.destroy()
+                console.error('Error getting notepad reputations: ' + error.message)
+            } else {
+                var setReputation = true;
+                // Display reputation for EICAR
+                for (var i in notepadReputation) {
+                    // It is only take into account the reputation of official providers and not the external provider's reputation
+                    if (i != 11) {
+                        // if the oficial providers do not have a reputation for the specified file or is unknown and the external reputation does not
+                        // have conflicts with the oficial provider's reputation, then the reputation can be set
+                        if (notepadReputation[i].trustLevel != 0 && notepadReputation[i].trustLevel != 50 && notepadReputation[i].trustLevel != externalTrustLevel) {
+                            setReputation = false;
+                        }
+                    }
+                }
+                if (setReputation) {
+                    tieClient.setExternalFileReputation(
+                        function() {
+                            // Destroy the client - frees up resources so that the application
+                            // stops running
+                            client.destroy();
+                            console.log('Event Sent')
+                        },
+                        TrustLevel.KNOWN_TRUSTED,
+                        18,
+                        hashes,
+                        'notepad.exe',
+                        'Reputation set via OpenDXL'
+                    )
+                } else {
+                    console.error('Error setting reputation: The reputation you try to set has conflicts with the current reputation');
+                    client.destroy();
+                }
+            }
         },
-        TrustLevel.KNOWN_TRUSTED,
-        hashes,
-        'notepad.exe',
-        'Reputation set via OpenDXL'
+        hashes
     )
 })
