32d7ad7f698aae66f82574dd5bb2dec42775f6d5
vrevigli <Valentin_ReviglioCanepa@McAfee.com>
2019-03-11 10:36:53 -0300

    Some improves over the 'set external file reputation' sample were made

--- sample/basic/basic-set-external-file-reputation-example.js
+++ sample/basic/basic-set-external-file-reputation-example.js
@@ -14,23 +14,20 @@ var config = dxl.Config.createDxlConfigFromFile(common.CONFIG_FILE)
 // Create the client
 var client = new dxl.Client(config)
 
-// Hashes for the file whose reputation should be set. These use the hashes for
-// notepad.exe by default but could be replaced with appropriate values for the
-// file whose reputation should be set.
-var FILE_MD5 = 'f2c7bb8acc97f92e987a2d4087d021b1'
-var FILE_SHA1 = '7eb0139d2175739b3ccb0d1110067820be6abd29'
-var FILE_SHA256 = '142e1d688ef0568370c37187fd9f2351d7ddeda574f8bfa9b0fa4ef42db85aa2'
-
 // Connect to the fabric, supplying a callback function which is invoked
 // when the connection has been established
 client.connect(function() {
     var tieClient = new TieClient(client)
         // The trust level that the external provider wants to set to a especific file
     var externalTrustLevel = TrustLevel.KNOWN_TRUSTED
+    
+    // Hashes for the file whose reputation should be set. These use the hashes for
+    // notepad.exe by default but could be replaced with appropriate values for the
+    // file whose reputation should be set.
     var hashes = {}
-    hashes[HashType.MD5] = 'e872f1695c189c1efa656038073aa310' //FILE_MD5
-    hashes[HashType.SHA1] = 'bd3987cd8242f4e50fb99b120f16be049ddb3685' //FILE_SHA1
-    hashes[HashType.SHA256] = '84e97beb57b6410a5e8ba7871907a95b42dc78afef07e9da66ba1d8c05abffd3' //FILE_SHA256
+    hashes[HashType.MD5] = 'f2c7bb8acc97f92e987a2d4087d021b1' //FILE_MD5
+    hashes[HashType.SHA1] = '7eb0139d2175739b3ccb0d1110067820be6abd29' //FILE_SHA1
+    hashes[HashType.SHA256] = '142e1d688ef0568370c37187fd9f2351d7ddeda574f8bfa9b0fa4ef42db85aa2' //FILE_SHA256
 
     // Request reputation for notepad
     tieClient.getFileReputation(
@@ -48,8 +45,9 @@ client.connect(function() {
                     if (i != 11) {
                         // if the oficial providers do not have a reputation for the specified file or is unknown and the external reputation does not
                         // have conflicts with the oficial provider's reputation, then the reputation can be set
-                        if (notepadReputation[i].trustLevel != 0 && notepadReputation[i].trustLevel != 50 && notepadReputation[i].trustLevel != externalTrustLevel) {
+                        if (notepadReputation[i].trustLevel != 0 && notepadReputation[i].trustLevel != 50) {
                             setReputation = false;
+                            break;
                         }
                     }
                 }
